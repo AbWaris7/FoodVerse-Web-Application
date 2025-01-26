2f97123dcd7275dee8150aaa0db12495b062f33f
abdulwariskhan <hellokhan.ch@gmail.com>
2025-01-26 11:34:54 +0100

    Makeing the Mobile Navigation Work

--- js/script.js
+++ js/script.js
@@ -2,3 +2,10 @@ const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
 console.log(currentYear);
 yearEl.textContent = currentYear;
+
+// Make Mobile Navigation work
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
